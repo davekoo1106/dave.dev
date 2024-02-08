00f4c937f7809ad3ac80ec32e86108d65305a78e
davekoo1106 <114951120+davekoo1106@users.noreply.github.com>
2024-02-07 23:40:27 -0800

    Created Project Cards with hover effects

---
+++ app/constants/projectContent.js
@@ -0,0 +1,19 @@
+export const projectContent = [
+    {
+        id:1,
+        name: "RxPress",
+        slug: "rxpress",
+        blurb: "Prescriptions delivered to your doorstep",
+        description: "RxPress is a web platform designed to seamlessly connect doctors, patients and pharmacies. Everything done at your fingertips and your prescriptions delivered to your door",
+        projectUrl: "",
+        imageContent: ""
+    }
+
+
+
+
+
+
+
+
+]
